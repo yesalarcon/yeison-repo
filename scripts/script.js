624347914b209c05b9ac4184280227fffa853fe5
davidrzuluaga <davidricardo2003@gmail.com>
2020-06-01 23:04:48 -0500

    function to color

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,6 @@
-function changePtoBlue() {
+function changePtoColor(color = "black") {
     let allP = document.querySelectorAll("p");
     for (let i = 0; i < allP.length; i++) {
-        allP[i].style.color = "blue"
-    }
-}
-
-function changePtoBlack() {
-    let allP = document.querySelectorAll("p");
-    for (let i = 0; i < allP.length; i++) {
-        allP[i].style.color = "black"
+        allP[i].style.color = color
     }
 }
